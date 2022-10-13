61d1789bc63b9b348947d8914299999bc44ad47b
ae_lkruk <ae_lkruk@idcontractors.net>
2022-10-12 23:52:13 -0300

    Se agrega fetch y map

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Routes, Route } from "react-router-dom";
 import { BarraNav } from './components/NavBar';
 import './App.css';
-import { ItemListContainer } from './components/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
       <Routes>
         <Route exact path= "/" element = {<ItemListContainer/>} />
         <Route exact path= "/category/:id" element = {<ItemListContainer/>} />
-        <Route exact path= "/item/:id" element = {<ItemDetailContainer/>} />
       </Routes>
     </BrowserRouter>
   );
