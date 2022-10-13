36df8d5f474b46911fc4070dbfe5cf1eefbead4c
ae_lkruk <ae_lkruk@idcontractors.net>
2022-10-12 23:52:13 -0300

    Se agrega fetch y map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 import { BarraNav } from './components/NavBar';
 import './App.css';
 import { ItemListContainer } from './components/ItemListContainer';
 
 function App() {
   return (
-    <article>
+    <BrowserRouter>
       <BarraNav/>
-      <ItemListContainer/>
-    </article>
+      <Routes>
+        <Route exact path= "/" element = {<ItemListContainer/>} />
+        <Route exact path= "/category/:id" element = {<ItemListContainer/>} />
+        <Route exact path= "/item/:id" element = {<ItemDetailContainer/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,3 +1,28 @@
-export const ItemListContainer = () => {
-    return <h1>Bienvenidos al vivero Iguazu</h1>
+import { useEffect, useState } from "react";
+import { getFetch } from "../helpers/getFetch"
+
+
+const ItemListContainer = () => {
+    const [ productos, setProductos ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+    
+    useEffect ( ()=> {
+        getFetch('test')
+        .then(resp => setProductos(resp))
+        .catch(err => console.log(err))
+        .finally( () => setLoading(false));
+
+    }, [])
+    
+    console.log(productos);
+    
+    return (
+        <>
+        <h1>Bienvenidos al vivero Iguazu</h1>
+        { loading ? <h2>Cargando...</h2> :
+        productos.map(producto => <li key={producto.id}>{producto.name}</li>)}
+        </>
+    )
 } 
+
+export default ItemListContainer
